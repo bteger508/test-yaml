135a2522aba1b5dfdb9db999c351a154876d75c7
bteger508 <bteger28@gmail.com>
2021-10-10 20:33:46 -0400

    Add test for sub()

--- calculator_test.py
+++ calculator_test.py
@@ -9,3 +9,9 @@ class PTestClass:
     def test(self):
         expected = 10
         assert_equals(add(4, 6), expected)
+
+    @Test(tags=["sub"])
+    def test(self):
+        expected = 10
+        assert_equals(sub(14, 4), expected)
+        
